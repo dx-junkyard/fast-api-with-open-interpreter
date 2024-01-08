65d7608afd5ae0b34db73c49bd9ef4cb029030a5
FooQoo <horseshoes.buzz.17@gmail.com>
2024-01-08 12:11:49 +0900

    feat: open-interpreterの追加

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import interpreter
+import os
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+interpreter.model = f"azure/{os.environ['AZURE_API_DEPLOYMENT_ID']}"
+interpreter.auto_run = True  # ユーザーの確認なしで生成されたコードを自動的に実行できるようになる
+interpreter.api_base = os.environ["AZURE_API_BASE"]
+interpreter.api_key = os.environ["AZURE_API_KEY"]
+interpreter.api_version = os.environ["AZURE_API_VERSION"]
+interpreter.debug_mode = False
+interpreter.temperature = 1.0
+interpreter.conversation_history = False
+
+
+origins = [
+    "http://localhost:3000",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -13,3 +39,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/chat")
+def chat_endpoint(message: str):
+    def event_stream():
+        for result in interpreter.chat(message, stream=True):
+            resultJson = json.dumps(result, ensure_ascii=False)
+            yield f"data: {resultJson}\n\n"
+
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
+
+
+@app.get("/history")
+def history_endpoint():
+    return interpreter.messages
